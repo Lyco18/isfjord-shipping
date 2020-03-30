07b16b19b38ef30d864ec0f9513ca0aaaa6b0dea
Lyco18 <lydiacocozza@hotmail.com>
2020-03-31 09:28:02 +1000

    homepage update

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar-inner">
     <a href="{{ asset('home') }}">Home</a>
     <a href="{{ asset('malmo') }}">Malmö</a>
-    <a href="{{ asset('charter') }}">Charter Our Ships</a>
+    <a href="{{ asset('charter') }}">Charter Our Ship</a>
     <a href="{{ asset('options') }}">Cruise Options</a>
     <a href="{{ asset('contact') }}">Contact</a>
     <a href="{{ asset('gallery') }}">Gallery</a>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.default')
 @section('content')
-<div class="home-body full-height"></div>
+<div class="front-image  full-height">
+    <img src="{{ asset('../public/storage/img/fortojdisen-5376.JPG') }}"></img>
+</div>
 <div class="flex-center position-ref">
     <div class="content">
         <div class="title m-b-md">
             Isfjord Shipping AB - Home
         </div>
     </div>
-</div>
 </div>
 @stop

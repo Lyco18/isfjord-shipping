c9d66c66431e591f77017165995c2f3b60148283
Lyco18 <lydiacocozza@hotmail.com>
2020-06-10 16:44:30 +1000

    css homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 <div class="front-image  full-height">
     <img src="{{ asset('storage/img/fortojdisen-5376.JPG') }}"></img>
 </div>
-<div class="flex-center position-ref">
+<div class="flex-center position-ref full-height">
     <div class="content">
         <div class="title m-b-md">
             Isfjord Shipping AB - Home
